2625ff13659dabfd4e7fe4eee3255819b1554139
Mizue Ando <mzeand33@gmail.com>
2025-11-23 23:20:02 +0900

    4.3 No. 3

---
+++ chapter4/4_3_3/matchmonitor.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"sync"
+	"time"
+)
+
+func matchReader(matchEvents *[]string, mutex *sync.RWMutex) {
+	for i := 0; ; i++ {
+		if mutex.TryLock() {
+			*matchEvents = append(*matchEvents, "Match event"+strconv.Itoa(i))
+			mutex.Unlock()
+			fmt.Println("Append match event", i)
+		} else {
+			fmt.Println("Failed to acquire lock, skipping event", i)
+		}
+		time.Sleep(200 * time.Millisecond)
+	}
+}
+
+func clientHandler(mEvents *[]string, mutex *sync.RWMutex, st time.Time) {
+	for i := 0; i < 100; i++ {
+		if mutex.TryRLock() {
+			allEvents := copyAllEvents(mEvents)
+			mutex.RUnlock()
+			timeTaken := time.Since(st)
+			fmt.Println(len(allEvents), "events copied in", timeTaken)
+		} else {
+			fmt.Println("Failed to acquire read lock, skipping copy", i)
+		}
+		time.Sleep(200 * time.Millisecond)
+	}
+}
+
+func copyAllEvents(matchEvents *[]string) []string {
+	allEvents := make([]string, 0, len(*matchEvents))
+	for _, event := range *matchEvents {
+		allEvents = append(allEvents, event)
+	}
+	return allEvents
+}
+
+func main() {
+	mutex := sync.RWMutex{}
+	var matchEvents = make([]string, 0, 10000)
+	for j := 0; j < 10000; j++ {
+		matchEvents = append(matchEvents, "Match event")
+	}
+
+	go matchReader(&matchEvents, &mutex)
+	startTime := time.Now()
+	go clientHandler(&matchEvents, &mutex, startTime)
+	time.Sleep(100 * time.Second)
+}
