a9f1baba07baf45275b938e20a51c3db7729d981
Mizue Ando <mzeand33@gmail.com>
2025-11-23 20:32:27 +0900

    3.4 No. 1

---
+++ chapter3/3_4_1/wordfrequency.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func countWords(url string, frequency map[string]int) {
+	fmt.Println("Fetching URL:", url)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error fetching URL:", err)
+		return
+	}
+	defer resp.Body.Close()
+	var word string
+	for {
+		_, err := fmt.Fscan(resp.Body, &word)
+		if err != nil {
+			break
+		}
+		frequency[word]++
+	}
+
+}
+
+func main() {
+	var frequency = make(map[string]int)
+
+	for i := 1000; i < 1010; i++ {
+		url := fmt.Sprintf("https://www.rfc-editor.org/rfc/rfc%d.txt", i)
+		go countWords(url, frequency)
+	}
+
+	time.Sleep(1 * time.Second)
+	for word, freq := range frequency {
+		if freq > 0 {
+			fmt.Printf("%s -> %d\n", word, freq)
+		}
+	}
+
+}
